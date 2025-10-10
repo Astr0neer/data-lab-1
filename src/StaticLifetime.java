b28ff864e02eaa5dca5d8148b78db761aa546f90
Astr0neer <mstbtknbs@gmail.com>
2025-10-10 15:25:15 +0300

    "First commit"

---
+++ src/StaticLifetime.java
@@ -0,0 +1,17 @@
+public class StaticLifetime {
+    static int counter =0;
+
+    void incrementCount() {
+        counter++;
+        System.out.println("Counter = " + counter);
+    }
+
+    public static void main(String[] args) {
+        StaticLifetime obj1 = new StaticLifetime();
+        StaticLifetime obj2 = new StaticLifetime();
+
+        obj2.incrementCount();
+        obj1.incrementCount();
+
+    }
+}
