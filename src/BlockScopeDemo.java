e29949565de7ff427185387086dfd13d5e328887
Astr0neer <mstbtknbs@gmail.com>
2025-10-10 15:51:05 +0300

    "First commit"

---
+++ src/BlockScopeDemo.java
@@ -0,0 +1,14 @@
+public class BlockScopeDemo {
+    public static void main(String[] args) {
+        int outerX = 5;
+        if(outerX ==5){
+            int innerY = 15;
+            System.out.println("Inside if block: outerX= " + outerX  +" " + "InnerY= " + innerY);
+        }
+
+        System.out.println(outerX);
+
+    }
+
+
+}
